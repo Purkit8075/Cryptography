b7c5bca1c106afa52a56c27016773a73b0e2b1d3
Purkit <purkit@outlook.com>
2023-05-28 12:29:12 +0800

    edit cipher.cpp

--- cipher.cpp
+++ cipher.cpp
@@ -6,21 +6,4 @@ using namespace std;
 
 int main(void)
 {
-	using std::cout;
-	PlayFair playfair;
-	// playfair.GenerateBox();
-	// playfair.ShowBox();
-	// playfair.WriteBoxToFile();
-	// std::cout << playfair.Preprocess("aabc13");
-	playfair.ReadinBoxFromFile("./test.txt");
-	// std::cout << playfair.EnCode("FK");
-	// playfair.GenerateBox();
-	// playfair.ShowBox();
-	string s = "dkayfnirmofnir";
-	// playfair.GenerateAlphaBox();
-	// playfair.ShowAlphaBox();
-	// cout << endl;
-	// cout << playfair.Preprocess(s) << endl;
-	cout << playfair.Preprocess(s) << endl;
-	cout << playfair.EnCode(s);
 }
